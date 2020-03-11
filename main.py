b9a2c60b7751ee31809b58c1ced2378b03d09e51
n0tr3dporegranate <270775mik@gmail.com>
2020-03-11 11:13:22 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import win32api
+from pathlib import Path
+from subprocess import Popen, CREATE_NEW_CONSOLE
+
+width = win32api.GetSystemMetrics(0)
+
+
+def dir_content(path):
+    try:
+        win32api.SetCursorPos((width, 0))
+    except:
+        pass
+    try:
+        print(str(path), width)
+    except UnicodeEncodeError:
+        pass
+
+    if Path(path).is_dir():
+        try:
+            for entry in Path(path).iterdir():
+                dir_content(entry)
+        except PermissionError:
+            pass
+
+
+p = Popen("cmd /c " + sys.executable, creationflags=CREATE_NEW_CONSOLE)
+
+os.system('color 2')
+dir_content("C:/")
+
+input()
